e25c80f8a19f38ee032984dcf6d74c38081d2cf3
Daniel Cajas <danielsebascm@hotmail.com>
2023-05-02 11:44:57 -0600

    replicated racket implementation

--- lexer.h
+++ lexer.h
@@ -1,29 +1,73 @@
 #include <iostream>
 #include <vector>
 #include <regex>
+#include <map>
 
 using namespace std;
+
+string operatorSafe(string s)
+{
+    return "<[^/>]*>" + s + "</[^>]*>";
+}
+
+enum TokenType
+{
+    KEYWORD,
+    OPERATOR_OR_PUNCTUATOR,
+    INTEGER_LITERAL,
+    REAL_LITERAL,
+    STRING_LITERAL,
+    CHARACTER_LITERAL,
+    COMMENT
+};
+
+map<TokenType, string> token_class_name = {
+    {OPERATOR_OR_PUNCTUATOR, "operatorOrPunctuator"},
+    {KEYWORD, "keyword"},
+    {INTEGER_LITERAL, "integerLiteral"},
+    {REAL_LITERAL, "realLiteral"},
+    {STRING_LITERAL, "stringLiteral"},
+    {CHARACTER_LITERAL, "characterLiteral"},
+    {COMMENT, "comment"}};
 
 class Lexer
 {
 private:
-    vector<string> keywords = {"abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface", "internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override", "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while"};
-    vector<string> operators = {"+", "-", "*", "/", "%", "++", "--", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "^", "~", "<<", ">>", ">>>", "<<<", "=", "+=", "-=", "*=", "/=", "%=", "<<=", ">>=", ">>>=", "<<<=", "&=", "|=", "^=", "=>", ".", "::", ":", "?", "??", "??=", "->", "=>", "=>>", "=>>>", "=>>>", "=>>=", "=>>>=", "=>>>=", "=>>>="};
-    vector<string> punctuation = {",", ";", "(", ")", "{", "}", "[", "]", ":", "::", ".", "?", "??", "??=", "->", "=>", "=>>", "=>>>", "=>>>", "=>>=", "=>>>=", "=>>>=", "=>>>="};
-    vector<pair<regex, string>> token_patterns = {
-        {regex("\\b(if|else|while|for|switch|case|break|continue|return)\\b"), "keyword"},
-        {regex("\\b(\\w+)\\b"), "identifier"},
-        {regex("\\b(true|false|null)\\b"), "literal"},
-        {regex("\\b(\\d+)\\b"), "literal"},
-        {regex("\\b(0x[\\da-fA-F]+)\\b"), "literal"},
-        {regex("\\b([\\d.]+f?)\\b"), "literal"},
-        {regex("[+\\-*/%<>=!&|\\^~?]+"), "operator"},
-        {regex("[;{}\\(\\)\\[\\],.]"), "punctuation"}
-    };
+    vector<pair<TokenType, regex>> token_regexes = {
+
+        {COMMENT,
+         regex("(" + operatorSafe("//") + ".* | " + operatorSafe("/\\*") + ".*" + operatorSafe("\\*/") + ")")},
+        {KEYWORD,
+         regex("\\b(abstract|as|base|bool|break|byte|case|catch|char|checked|const|continue|decimal|default|delegate|do|double|else|enum|event|explicit|extern|false|finally|fixed|float|for|foreach|goto|if|implicit|in|int|interface|internal|is|lock|long|namespace|new|null|object|operator|out|override|params|private|protected|public|readonly|ref|return|sbyte|sealed|short|sizeof|stackalloc|static|string|struct|switch|this|throw|true|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|virtual|void|volatile|while)\\b")},
+        {REAL_LITERAL,
+         regex("[0-9]+\\" + operatorSafe(".") + "[0-9]+")},
+        {INTEGER_LITERAL,
+         regex("[0-9]+")},
+        {STRING_LITERAL,
+         regex("\"[^\"]*\"")},
+        {CHARACTER_LITERAL,
+         regex("'[^']*'")}};
+
 public:
-    Lexer();
-    ~Lexer();
-    vector<string> tokenize(string input);
+    Lexer() = default;
+    ~Lexer() = default;
+    string lex(string);
+    string wrap(string, string);
 };
 
+string Lexer::lex(string input)
+{
+    for (auto token_regex : token_regexes)
+    {
+        cout << token_class_name[token_regex.first] << endl;
+        input = regex_replace(input, token_regex.second, wrap("$&", token_class_name[token_regex.first]));
+    }
 
+    return "<head> <link rel='stylesheet' href='styles.css'> <title>problemSituation</title> </head><style>span *{color:inherit !important}</style><pre>" + input +
+           "</pre>";
+}
+
+string Lexer::wrap(string input, string class_name)
+{
+    return "<span class=" + class_name + ">" + input + "</span>";
+}
--- main.cpp
+++ main.cpp
@@ -1,33 +1,21 @@
 #include <regex>
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
+#include "lexer.h"
 
-std::string regex_escape(const std::string& str) {
-    static const std::regex re(R"([-[\]{}()*+?.,\^$|#\s])");
-    return std::regex_replace(str, re, R"(\$&)");
-}
+int main()
+{
+    Lexer lexer;
+    ifstream file("input.cs");
+    ofstream output("output.html");
+    string input((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
 
-int main() {
-    std::vector<std::string> keywords = {"foo", "bar", "baz"};
-    std::string pattern = "(?![^<]*>)\\b(";
-    for (auto kw : keywords) {
-        pattern += regex_escape(kw) + "|";
-    }
-    pattern.pop_back();
-    pattern += ")\\b";
-    std::regex keywordRegex(pattern);
-    std::string testString = "This is a test string with foo, bar, and baz.";
-    std::smatch match;
-    std::ofstream outFile("output.html");
-    outFile << "<html><head><title>Keyword Highlighter</title><style>.keyword{color:red;}</style></head><body><p>";
-    while (std::regex_search(testString, match, keywordRegex)) {
-        outFile << match.prefix();
-        outFile << "<span class=\"keyword\">" << match[0] << "</span>";
-        testString = match.suffix();
-    }
-    outFile << testString << "</p></body></html>";
-    outFile.close();
+    string outputText = lexer.lex(input);
+
+    output << outputText;
+
+    file.close();
+    output.close();
+
     return 0;
 }
