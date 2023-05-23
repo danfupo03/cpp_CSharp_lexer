5cb753bb8c4d27d2ebbb12216aad2ecfd80594e4
danfupo03 <danfupo@outlook.com>
2023-05-22 19:51:05 -0600

    Erased some files

--- src/realLiteral.h
+++ src/realLiteral.h
@@ -11,25 +11,6 @@ RealLiteralHighlighter::RealLiteralHighlighter()
 {
     className = "realLiterals";
 
-    // (define operatorSafe (lambda (s) (string-append "(<[^/>]*>" s "</[^>]*>)")))
-
-    // (define decimalDigit "[0-9]")
-
-    // (define decoratedDecimalDigit (string-append "(_*" decimalDigit ")"))
-
-    // (define realTypeSuffix "[FfDdMm]")
-
-    // (define sign (operatorSafe "(\\+|-)"))
-
-    // (define exponentPart (string-append "([eE]" sign "?" decimalDigit decoratedDecimalDigit "*)"))
-
-    // (define realLiteral
-    //   (string-append
-    //    "\\s(" decimalDigit decoratedDecimalDigit "*" (operatorSafe "\\.") decimalDigit decoratedDecimalDigit "*" exponentPart "?" realTypeSuffix "?|"
-    //    (operatorSafe "\\.") decimalDigit decoratedDecimalDigit "*" exponentPart "?" realTypeSuffix "?|"
-    //    decimalDigit decoratedDecimalDigit "*" exponentPart realTypeSuffix "?|"
-    //    decimalDigit decoratedDecimalDigit "*" realTypeSuffix ")\\b" ))
-
     std::string decimalDigit = "[0-9]";
     std::string decoratedDecimalDigit = "(_*" + decimalDigit + ")";
     std::string realTypeSuffix = "[FfDdMm]";
